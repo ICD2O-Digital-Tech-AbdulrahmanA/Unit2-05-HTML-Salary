fc9dc458f92767debc707ce83fcc6efdd7abfb35
Abdulrahman Albinali <abdulrahman.albinali@stu.ocsb.ca>
2025-04-02 15:00:02 +0000

    Salary Program

--- js/script.js
+++ js/script.js
@@ -2,4 +2,17 @@
 //
 // Created by: Mr. Coxall
 // Created on: Sep 2020
-// This file contains the JS functions for index.html
+// This file contains the JS functions for index.htm
+function hoursWorked() {
+    const TAX = 20.05;
+    let hoursWorked = parseInt(document.getElementById('hours-worked').value)
+    let hourlyRate = parseFloat(document.getElementById('hourly-rate').value)
+    let gross = hoursWorked * hourlyRate;
+    let govPay = gross * TAX / 100;
+    let net = gross - govPay;
+
+    document.getElementById('gross-pay').innerHTML = "Your Gross Pay is " + gross.toFixed(2) + "$";
+    document.getElementById('net-pay').innerHTML = "Your Net Pay is " + net.toFixed(2) + "$";
+    document.getElementById('gov-pay').innerHTML = "The Government takes " + govPay.toFixed(2) + "$";
+
+}
